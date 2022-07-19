eeff08bd6a8febc2005b8627555d6e9f498e5a3f
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-19 16:47:07 +0530

    added newArrivals config

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,26 @@
+import { SwiperOptions } from 'swiper';
 export class ConstantsClass {
-    static webHeight = '90vh';
-    static imgWidth = '100%';
-    static imgHeight = "800px";
+  static webHeight = '90vh';
+  static imgWidth = '100%';
+  static imgHeight = '100%';
+  static config: SwiperOptions = {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      clickable: true,
+    },
+  };
+
+  static newArrivalsconfig: SwiperOptions = {
+    spaceBetween: 50,
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    loop: true,
+    // pagination : true,
+    navigation: true,
+  };
 }
