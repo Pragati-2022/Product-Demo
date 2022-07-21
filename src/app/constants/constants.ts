25bffb050a174686b15e7b27079ad0d0f0ca6a80
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-21 10:26:02 +0530

    added bestSellersConfig

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -23,4 +23,14 @@ export class ConstantsClass {
     // pagination : true,
     navigation: true,
   };
+
+  static bestSellersConfig: SwiperOptions = {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  };
 }
