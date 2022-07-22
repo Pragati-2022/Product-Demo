22ab271cb5210e0e6d0332e2ae28adc1bedc6539
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-22 16:53:04 +0530

    added slider

--- src/app/pages/home/best-sellers/best-sellers.component.ts
+++ src/app/pages/home/best-sellers/best-sellers.component.ts
@@ -24,8 +24,31 @@ export class BestSellersComponent implements OnInit {
       author: 'Nidhi',
     },
   ];
+
+  images = [
+    {
+      image:
+        'https://cdn.ainabys4u.com/cms/e45161c316cb480ea6f5e88fe0d516df.jpg',
+    },
+    {
+      image:
+        'https://cdn.ainabys4u.com/cms/5786276306514616b6e18d7c70f78f15.jpg',
+    },
+    {
+      image:
+        'https://cdn.ainabys4u.com/cms/427e7a688e684ca3a71c2071c7d96729.jpg',
+    },
+    {
+      image:
+        'https://cdn.ainabys4u.com/cms/9c1468b611024ab1bbe97768c1e0b9e5.jpg',
+    },
+    {
+      image:
+        'https://cdn.ainabys4u.com/cms/1636c280ed774c7ba13c0850f08bd206.jpg',
+    },
+  ];
   constructor() {
-    this.constant = ConstantsClass
+    this.constant = ConstantsClass;
   }
 
   ngOnInit(): void {}
