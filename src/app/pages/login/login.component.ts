99fec08e14c206a0563b07a8aad1d1941c6ccaca
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-25 16:18:37 +0530

    login and signup functionality with firebase

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,12 +2,14 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import {
   ConfirmValidParentMatcher,
   CustomValidator,
 } from 'src/app/services/login/custom.validator';
 import { LoginService } from 'src/app/services/login/login.service';
-import { SignUpComponent } from '../sign-up/sign-up.component';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +26,9 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder,
     private loginService: LoginService,
     private router: Router,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private auth: AuthService,
+    private toast: HotToastService
   ) {
     this.initializeForm();
   }
@@ -55,64 +59,98 @@ export class LoginComponent implements OnInit {
     };
 
     if (!this.NotFoundUser) {
-      if (
-        this.loginService.users.filter(
-          (userData) =>
-            userData.email === user.email && userData.password === user.password
-        ).length
-      ) {
-        this.router.navigate(['home']);
-        console.log(
-          this.loginService.users.filter(
-            (userData) =>
-              userData.email === user.email &&
-              userData.password === user.password
-          )
-        );
+      // if (
+      //   this.loginService.users.filter(
+      //     (userData) =>
+      //       userData.email === user.email && userData.password === user.password
+      //   ).length
+      // ) {
+      //   this.router.navigate(['home']);
+      //   this.auth.login(user.email, user.password);
+      //   console.log(
+      //     this.loginService.users.filter(
+      //       (userData) =>
+      //         userData.email === user.email &&
+      //         userData.password === user.password
+      //     )
+      //   );
 
-        alert('home');
-        this.dialog.closeAll();
-        return;
-      } else {
-        console.log(
-          this.loginService.users.filter(
-            (userData) =>
-              userData.email === user.email &&
-              userData.password === user.password
-          )
-        );
+      //   alert('home');
+      //   this.dialog.closeAll();
+      //   return;
+      // } else {
+      //   console.log(
+      //     this.loginService.users.filter(
+      //       (userData) =>
+      //         userData.email === user.email &&
+      //         userData.password === user.password
+      //     )
+      //   );
 
-        this.NotFoundUser = true;
-        this.loginForm.addValidators(CustomValidator.passwordMatch);
-        this.loginForm.updateValueAndValidity();
-        alert('signup');
-      }
+      //   this.NotFoundUser = true;
+      //   this.loginForm.addValidators(CustomValidator.passwordMatch);
+      //   this.loginForm.updateValueAndValidity();
+      //   alert('signup');
+      // }
+
+      this.auth
+        .login(user.email, user.password)
+        .pipe(
+          this.toast.observe({
+            success: 'Logged in successfully',
+            loading: 'Logging in...',
+            error: ({ message }) => `There was an error: ${message}`,
+          })
+        )
+        .subscribe(
+          (result) => {},
+          (error) => {
+            this.NotFoundUser = true;
+            this.loginForm.addValidators(CustomValidator.passwordMatch);
+            this.loginForm.updateValueAndValidity();
+            console.log(error);
+          },
+          () => {
+            this.router.navigate(['home']);
+            this.dialog.closeAll();
+          }
+        );
     } else {
-      if (
-        this.loginService.users.filter(
-          (userData) => userData.email === user.email
-        ).length
-      ) {
-        alert('user already exist!');
-        return;
-      }
+      // if (
+      //   this.loginService.users.filter(
+      //     (userData) => userData.email === user.email
+      //   ).length
+      // ) {
+      //   alert('user already exist!');
+      //   return;
+      // }
 
-      this.loginService.addUser(user);
-      this.NotFoundUser = false;
-      this.loginForm.clearValidators();
-      this.loginForm.updateValueAndValidity();
+      // this.loginService.addUser(user);
+      // this.auth.createUser(user);
+      // this.NotFoundUser = false;
+      // this.loginForm.clearValidators();
+      // this.loginForm.updateValueAndValidity();
+
+      this.auth
+        .createUser(user)
+        .pipe(
+          this.toast.observe({
+            success: 'Congrats! You are all signed up',
+            loading: 'Signing up...',
+            error: ({ message }) => `${message}`,
+          })
+        )
+        .subscribe(
+          (result) => {},
+          (error) => {
+            console.log(error);
+          },
+          () => {
+            this.NotFoundUser = false;
+            this.loginForm.clearValidators();
+            this.loginForm.updateValueAndValidity();
+          }
+        );
     }
-    // this.dialog.closeAll();
-    // this.dialog.open(LoginComponent, {
-    //   maxWidth: '100vw',
-    //   maxHeight: '100vh',
-    //   height: '50%',
-    //   width:
-    //     this.screenWidth < 450
-    //       ? '75%'
-    //       : this.screenWidth > 450 && this.screenWidth < 800
-    //       ? '400px'
-    //       : '500px',
-    // });
   }
 }
