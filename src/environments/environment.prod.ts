84f3ced435f95db16adcc504b8b7bd25586720bc
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-25 16:19:51 +0530

    firebaseConfig added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDElP8vTlb8J2CPbgBb6ABgVkUKM-G3su4",
+    authDomain: "fir-product-project.firebaseapp.com",
+    projectId: "fir-product-project",
+    storageBucket: "fir-product-project.appspot.com",
+    messagingSenderId: "146264746188",
+    appId: "1:146264746188:web:6ee342b8b54f10e062fa69",
+    measurementId: "G-072W6G5HCE"
+  }
 };
