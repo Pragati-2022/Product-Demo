50925c33f7237ffe3b5b94d217a1cf7afc2cde70
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-26 15:26:49 +0530

    changed array from this compoenent to product service

--- src/app/pages/home/image-slider/image-slider.component.ts
+++ src/app/pages/home/image-slider/image-slider.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ConstantsClass } from 'src/app/constants/constants';
+import { ProductService } from 'src/app/services/proudct/product.service';
 import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper';
 
 SwiperCore.use([Autoplay, Pagination, Navigation]);
@@ -11,31 +13,13 @@ SwiperCore.use([Autoplay, Pagination, Navigation]);
 export class ImageSliderComponent implements OnInit {
   public constant;
 
-  images = [
-    {
-      image:
-        'https://cdn.ainabys4u.com/cms/9751aa409005482ea1ee47bbf825dd51.jpg',
-    },
-    {
-      image:
-        'https://cdn.ainabys4u.com/cms/1a0dc6fc050c48ba9654a12dfd89665d.jpg',
-    },
-    {
-      image:
-        'https://cdn.ainabys4u.com/cms/15632fa549394fb29deaf13ccde420db.jpg',
-    },
-    {
-      image:
-        'https://cdn.ainabys4u.com/cms/06640f84b4df4064891d5ec5e619e717.jpg',
-    },
-    {
-      image:
-        'https://cdn.ainabys4u.com/cms/a6de9dbf90114ce2ba78a79fe8f6788d.jpg',
-    },
-  ];
-  constructor() {
+  constructor(public productService : ProductService, private router : Router) {
     this.constant = ConstantsClass;
   }
 
   ngOnInit(): void {}
+
+  onClick(product : any){
+    this.router.navigate(['product-details', product.id])
+  }
 }
