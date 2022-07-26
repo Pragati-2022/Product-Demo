f473733522943538667b88c7ac31c2a99e5a42cd
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-26 15:19:03 +0530

    added product-details/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailsComponent,
   },
 ];
 
